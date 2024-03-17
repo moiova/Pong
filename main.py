74453fec9e884a440c8c7609445e622a65e276c9
moiova <pingucom1@gmail.com>
2024-03-17 11:27:31 +0100

    TODO changes

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from turtle import Screen, Turtle
 
 # TODO create the GUI from multiple components:
 #  break down the components into separate classes
-#  two rackets consisting of two turtles build of two segment turtles
+#  two paddles consisting of two turtles build of two segment turtles
 #  one ball consisting of one turtle
 #  one scoreboard with a net
 
-# TODO create racket and make it onkey movable
+# TODO create paddle and make it onkey movable
 
 # TODO create the ball and make it movable
 
@@ -17,3 +17,13 @@ from turtle import Screen, Turtle
 # TODO detect collision with left and right wall (paddle miss)
 
 # TODO create the scoreboard and keep score
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.bgcolor("black")
+screen.tracer(0)  # animation off
+
+screen.update()
+
+screen.exitonclick()
