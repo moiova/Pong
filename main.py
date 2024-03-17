667ae305013ac53960c73f41a3e0d79b7dc7ae5f
moiova <pingucom1@gmail.com>
2024-03-17 11:09:45 +0100

    creating todo's for the game

---
+++ main.py
@@ -0,0 +1,19 @@
+from turtle import Screen, Turtle
+
+# TODO create the GUI from multiple components:
+#  break down the components into separate classes
+#  two rackets consisting of two turtles build of two segment turtles
+#  one ball consisting of one turtle
+#  one scoreboard with a net
+
+# TODO create racket and make it onkey movable
+
+# TODO create the ball and make it movable
+
+# TODO detect ball collision with wall and make it bounce
+
+# TODO detect collision with paddle and make the ball bounce back
+
+# TODO detect collision with left and right wall (paddle miss)
+
+# TODO create the scoreboard and keep score
