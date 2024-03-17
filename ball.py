4401a08da3305b164fbae6cb7020c3276d5bf06d
moiova <pingucom1@gmail.com>
2024-03-17 19:24:01 +0100

    creates new Ball class and move method

---
+++ ball.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+
+# create the ball and make it movable
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.color("white")
+
+    def move(self):
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
+        self.goto(new_x, new_y)
